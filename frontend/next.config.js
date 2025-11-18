40950fbe598f7e148c9d52009d7210977a1ab121
David Aiyewumi <davidaiyewumi1@gmail.com>
2025-11-18 09:40:36 +0100

    complete fix

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   images: {
     unoptimized: true,
   },
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
